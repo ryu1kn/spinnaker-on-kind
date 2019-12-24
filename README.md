49296e0ee5c36c6776c27c3516fd112254f13224
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-12-25 00:26:54 +1100

    Prefix spinnaker ver with `local:` to make it download image from local repository

--- README.md
+++ README.md
@@ -11,6 +11,12 @@
     helm repo add stable https://kubernetes-charts.storage.googleapis.com/
     ```
 
+## Bring up Spinnaker
+
+```sh
+make start-cluster apply-manifest
+```
+
 ## References
 
 * [kind](https://kind.sigs.k8s.io/)
--- scripts/localise-bom.sh
+++ scripts/localise-bom.sh
@@ -6,5 +6,7 @@ yaml_wrap() {
     yq read - -j | "$@" | yq read -
 }
 
-yaml_wrap jq '.services |= map_values(if .version then .version |= "local:\(.)" else . end)
+yaml_wrap jq '
+    .version |= "local:\(.)"
+    | .services |= map_values(if .version then .version |= "local:\(.)" else . end)
     | .artifactSources.dockerRegistry |= "registry:5000"'
