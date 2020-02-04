c9e5686dcf35fa44f2d58476e4b8744081921136
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-02-04 23:07:30 +1100

    Don't execute docker/kubectl/kind inside docker

--- makefiles/deployment.mk
+++ makefiles/deployment.mk
@@ -1,21 +1,13 @@
-is_inside_docker := $(shell [[ -f /proc/1/cgroup ]] && fgrep -m 1 docker /proc/1/cgroup)
-
-ifdef is_inside_docker
-KUBECTL_OPT := --kubeconfig <(sed 's/127\.0\.0\.1/host.docker.internal/g' ~/.kube/config) --insecure-skip-tls-verify
-endif
-
-KUBECTL := kubectl $(KUBECTL_OPT)
-
 .PHONY: apply-manifest delete-manifest
 apply-manifest delete-manifest: $(work_dir)/$(manifest)
-	$(KUBECTL) $(@:-manifest=) -f $<
+	kubectl $(@:-manifest=) -f $<
 
 .PHONY: wait-for-deployment-complete
 wait-for-deployment-complete:
-	until [[ "$$($(KUBECTL) get jobs $(helm_template_name)-install-using-hal -o jsonpath='{.status.succeeded}')" = 1 ]] ; do \
+	until [[ "$$(kubectl get jobs $(helm_template_name)-install-using-hal -o jsonpath='{.status.succeeded}')" = 1 ]] ; do \
 		echo "Waiting for the install job to complete..."; sleep 10; \
 	done
 
 .PHONY: expose-ui-port
 expose-ui-port:
-	$(KUBECTL) port-forward svc/spin-deck $(spinnaker_ui_port):9000
+	kubectl port-forward svc/spin-deck $(spinnaker_ui_port):9000
